3a76ceaf3b5e19a1a8431384dada4df0e7157d71
veis sadikov <veissadikov@veiss-MacBook-Pro.local>
2024-11-07 18:06:14 +0400

    Small fixes

--- contracts/JACK.sol
+++ contracts/JACK.sol
@@ -3,12 +3,14 @@ pragma solidity ^0.8.19;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {Ownable2Step} from "@openzeppelin/contracts/access/Ownable2Step.sol";
 
 
 
 
-contract JACK is ERC20, Ownable {
+contract JACK is ERC20, Ownable2Step {
     error MaxSupplyExceeded();
+    error OperationNotAllowed();
 
     uint256 constant public maxSupply = 1_000_000_000 ether;
 
@@ -19,5 +21,9 @@ contract JACK is ERC20, Ownable {
             revert MaxSupplyExceeded();
         }
         _mint(to, amount);
+    }
+
+    function renounceOwnership() public virtual override onlyOwner {
+        revert OperationNotAllowed();
     }
 }
